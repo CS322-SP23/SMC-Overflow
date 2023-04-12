dceb5fb1a82e48a3131dfe53836bb16552c317bb
svscumaci <81786239+svscumaci@users.noreply.github.com>
2023-04-12 18:36:40 +0000

    added profile page, linked button, linked back

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,static_folder="statics")
+app = Flask(__name__, static_folder="statics")
+app.config['DEBUG'] = True
 
 @app.route("/")
 def hello():
     return render_template("index.html")
-    
+
+@app.route('/profile_page')
+def profile_page():
+   return render_template("profile_page.html")
+
 @app.route('/form')
 def form():
-   return render_template('form.html')
+    return render_template('form.html')
+
 if __name__ == '__main__':
    app.run()
